76d7d7ed5e296163d40abd4ba2cd2eb68bd86bc3
Hassan Amin <hassanmamin994@gmail.com>
2017-04-09 21:46:50 +0200

    add menu image upload functionality to orders

--- src/app/orders/add-order/add-order.component.ts
+++ src/app/orders/add-order/add-order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FriendsService } from '../../friends/friends.service';
 import { GroupsService } from '../../groups/groups.service' ;
 import { OrdersService } from '../orders.service' ;
+import { FileToBase64Service } from '../../file-to-base64.service';
 @Component({
   selector: 'app-add-order',
   templateUrl: './add-order.component.html',
@@ -18,11 +19,16 @@ export class AddOrderComponent implements OnInit {
 	order = {
 		for: 'Lunch',
 		resturantname: '',
+		menu: '',
 		invite: '',
 		members: ''		
 	}
 	errors: Array<any> = [] ;
-	constructor(private friendsService: FriendsService, private groupsService: GroupsService, private ordersService: OrdersService) { }
+	constructor(private fileToBase64Service: FileToBase64Service, private friendsService: FriendsService, private groupsService: GroupsService, private ordersService: OrdersService) {
+		this.fileToBase64Service.fileToBase64Event.subscribe(
+				(base64Format: any) => {this.order.menu = base64Format ;console.log(this.order);}
+			)
+	}
 
 	ngOnInit() {
 	}
@@ -67,12 +73,34 @@ export class AddOrderComponent implements OnInit {
 			break ; 
 		}
 	}
-
+	selectMenu(input: any){
+		let allowed_extensions = ['image/jpeg','image/png','image/jpg'] ;
+		let file = input.files[0]; 
+		if(allowed_extensions.indexOf(file.type) != -1 ){
+			this.fileToBase64Service.convert(file);
+			this.errors = [] ;
+		}
+		else
+			this.errors.push("Only images with png, jpeg, jpg extensions are allowed");
+	}
+	getSelectedID(){
+		let selectedId ; 
+		if(this.order.invite == 'user'){
+			for( let i = 0 ; i < this.friendsList.length; i++ ){
+				if(this.order.members == this.friendsList[i].email)
+					selectedId =  this.friendsList[i]._id ;
+			}
+		}else{
+			for( let i = 0 ; i < this.groupsList.length; i++ ){
+				if(this.order.members == this.groupsList[i].name)
+					selectedId =  this.groupsList[i]._id ;
+			}				
+		}
+		return selectedId ;
+	}
 	onSubmit(){
 		// clear the errors array 
 		this.errors = [] ;
- 
-
 		// form validation 
 		for (var property in this.order) {
 		    if (this.order.hasOwnProperty(property)) {
@@ -84,19 +112,7 @@ export class AddOrderComponent implements OnInit {
 
 		if(this.errors.length == 0 ){
 			// get the selected id for group or friend/user
-			let selectedId ; 
-			if(this.order.invite == 'user'){
-				for( let i = 0 ; i < this.friendsList.length; i++ ){
-					if(this.order.members == this.friendsList[i].email)
-						selectedId =  this.friendsList[i]._id ;
-				}
-			}else{
-				for( let i = 0 ; i < this.groupsList.length; i++ ){
-					if(this.order.members == this.groupsList[i].name)
-						selectedId =  this.groupsList[i]._id ;
-				}				
-			}
-
+			let selectedId = this.getSelectedID();
 			// submit the form 
 			let orderObj = { 
 				name: this.order.for,
@@ -104,13 +120,14 @@ export class AddOrderComponent implements OnInit {
 				invited_type: this.order.invite ,
 				invited_id: selectedId
 			 }
-			 		console.log('order obj', orderObj);
+			console.log('order obj', orderObj);
 			this.ordersService.addOrder(JSON.stringify(orderObj)).subscribe(
-				(response: any) =>{
-					console.log(response);
+			(response: any) =>{
+				console.log(response);
+				if(!response.status){
+					this.errors = response.errors ;
 				}
-
-				);
+			});
 		}
 
 	}
--- src/app/orders/orders.service.ts
+++ src/app/orders/orders.service.ts
@@ -1,18 +1,22 @@
-import { Injectable } from '@angular/core';
+ import { Injectable } from '@angular/core';
 import { Http, Response ,Headers, RequestOptions} from '@angular/http';
 import { HeadersClass } from '../headers';
 import { ApiRouterService } from '../api-router.service';
-import { LocalstorageService } from '../localstorage.service';
 
 @Injectable()
 export class OrdersService extends HeadersClass {
 
-  constructor(private http: Http, private apiRouter: ApiRouterService, private localStorageService: LocalstorageService) {
-  	super(localStorageService) ;
+  constructor(private http: Http, private apiRouter: ApiRouterService) {
+  	super() ;
   }
 
 	addOrder(order: any){
 		return this.http.post(this.apiRouter.getOrdersRoute(),order,this.options)
+			.map((response: Response) => response.json());
+	}
+
+	getOrders(){
+		return this.http.get(this.apiRouter.getOrdersRoute(),this.options)
 			.map((response: Response) => response.json());
 	}
 }
