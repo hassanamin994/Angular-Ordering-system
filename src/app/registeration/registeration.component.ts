962b655b19651dac728d024b20ed67601ac8fbf2
Hassan Amin <hassanmamin994@gmail.com>
2017-04-09 23:12:42 +0200

    Add upload profile picture to registeration

--- src/app/registeration/registeration.component.ts
+++ src/app/registeration/registeration.component.ts
@@ -3,6 +3,7 @@ import { User } from '../User';
 import { RegisterationService } from './registeration.service';
 import { Response } from '@angular/http';
 import { Router} from '@angular/router';
+import { FileToBase64Service } from '../file-to-base64.service';
 @Component({
   selector: 'app-registeration',
   templateUrl: './registeration.component.html',
@@ -14,20 +15,35 @@ export class RegisterationComponent implements OnInit {
   user = {
     name:"",
     email:"",
+    image:"",
     password:"",
     confirm_password:"" 
   };
   errors = [] ;
   success_message : string = "" ;
-  constructor(private registerationService: RegisterationService, private router: Router) {
+  constructor(private fileToBase64Service: FileToBase64Service, private registerationService: RegisterationService, private router: Router) {
+    this.fileToBase64Service.fileToBase64Event.subscribe(
+        (base64Format: any) => {this.user.image = base64Format ;console.log(this.user);}
+      )
    }
 
   ngOnInit() {
+
+  }
+  selectImage(input: any){
+    let allowed_extensions = ['image/jpeg','image/png','image/jpg'] ;
+    let file = input.files[0]; 
+    if(allowed_extensions.indexOf(file.type) != -1 ){
+      this.fileToBase64Service.convert(file);
+      this.errors = [] ;
+    }
+    else
+      this.errors.push("Only images with png, jpeg, jpg extensions are allowed");
   }
   onSubmit(){
     if(this.user.name != "" && emailValidator(this.user.email) && this.user.password == this.user.confirm_password ){
-      let userObj = {"name": this.user.name, "email": this.user.email, "password": this.user.password, 'repassword': this.user.confirm_password };
-      this.registerationService.register(userObj).subscribe(
+      let userObj = {"name": this.user.name, "email": this.user.email, "password": this.user.password, 'repassword': this.user.confirm_password, image:this.user.image };
+      this.registerationService.register(JSON.stringify(userObj)).subscribe(
         (response: any) => {
           // if any errors occured view them to user
           if(!response.loggedIn){
