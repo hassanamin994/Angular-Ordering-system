58ee580e5dcd7cd32cc9830ab17665654103b556
Hassan Amin <hassanmamin994@gmail.com>
2017-04-14 23:33:18 +0200

    adding redirecting if guard fails

--- src/app/main-app.guard.ts
+++ src/app/main-app.guard.ts
@@ -1,10 +1,21 @@
-import { CanActivate , ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { CanActivate , ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs/Rx'
 import { LocalstorageService } from './localstorage.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
 export class MainAppGuard implements CanActivate{
-	
+	constructor(private router: Router) {}
+
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): Observable<boolean> | boolean{
 		let locastorageService: LocalstorageService = new LocalstorageService() ;
-		return locastorageService.isLoggedIn() ;
+		if(locastorageService.isLoggedIn()){
+			return true ;
+		}else{
+			this.router.navigate(['/login']);
+		    // abort current navigation
+		    return false;
+		}
 	}
 }
